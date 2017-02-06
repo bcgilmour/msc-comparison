a5de7c4a6b2dced256dc9078036084ee5676a348
bcgilmour <b.c.gilmour@outlook.com>
2017-02-05 23:42:20 -0700

    Update run.py

--- run.py
+++ run.py
@@ -1,15 +1,25 @@
 # x, y, total, income, date, rank, interest
 
+import stats
+
+# maybe this could fit in a class?
+
+# def create_selection():
+
 def run_prop(x_col, y_col, tot_col, prop):
+	return stats.proportion_calc(x_col, y_col, tot_col)
+	return stats.prop_weight(prop)
 
+def run_income(inc_col):
+	return stats.max_income(inc_col)
+	return stats.income_weight(inc_col)
 
-def run_income():
+def run_date(date_col):
+	return stats.pub_date(date_col)
+	return stats.pub_weight()
 
+def run_rank(rank_col):
+	return stats.level_weight(rank_col)
 
-def run_date():
-
-
-def run_rank():
-
-
-def run_interest():
+def run_interest(int_col):
+	return stats.interest_weight(int_col)
