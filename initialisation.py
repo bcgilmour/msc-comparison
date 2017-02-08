f77341837c79c4d0a5a0a28d93594f63db3213a9
bcgilmour <b.c.gilmour@outlook.com>
2017-02-07 19:04:17 -0700

    Update initialisation.py

--- initialisation.py
+++ initialisation.py
@@ -22,3 +22,35 @@ def create_dict(xlsx, options):
 		value_dict.update(lab.value: [])
 		for obj in xlsx.columns[2:options]:
 			value_dict[lab].append(obj.value)
+
+# defining the selection functions
+
+class Selection(object):
+	x_col = 2
+	y_col = 3
+	tot_col = 4
+	inc_col = 5
+	date_col = 6
+	int_col = 7
+	
+	def __init__(self, value_dict):
+		self.value_dict = value_dict
+		self.prop = []
+		
+	def run_prop(self, x_col, y_col, tot_col, prop):
+		return stats.proportion_calc(self.x_col, self.y_col, self.tot_col)
+		return stats.prop_weight(self.prop)
+
+	def run_income(self, inc_col):
+		return stats.max_income(self.inc_col)
+		return stats.income_weight(self.inc_col)
+
+	def run_date(self, date_col):
+		return stats.pub_date(self.date_col)
+		return stats.pub_weight()
+
+	def run_rank(self, rank_col):
+		return stats.level_weight(self.rank_col)
+
+	def run_interest(self, int_col):
+		return stats.interest_weight(self.int_col)
