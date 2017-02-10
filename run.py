6827888f7f339e35833c63cb4baeefca8ed06657
bcgilmour <b.c.gilmour@outlook.com>
2017-02-10 14:49:20 -0700

    Update run.py

--- run.py
+++ run.py
@@ -1,25 +1,17 @@
-# x, y, total, income, date, rank, interest
+import 'initialisation.py' as i
 
-import stats
+# importing data
 
-# maybe this could fit in a class?
+def define(file, name):
+	i.xlsx_import(file)
+	d = i.pop_dict(xlsx, options)
+	name = i.Selection(d)
+	run_stats(name)
+	print(name.prop)
 
-# def create_selection():
-
-def run_prop(x_col, y_col, tot_col, prop):
-	return stats.proportion_calc(x_col, y_col, tot_col)
-	return stats.prop_weight(prop)
-
-def run_income(inc_col):
-	return stats.max_income(inc_col)
-	return stats.income_weight(inc_col)
-
-def run_date(date_col):
-	return stats.pub_date(date_col)
-	return stats.pub_weight()
-
-def run_rank(rank_col):
-	return stats.level_weight(rank_col)
-
-def run_interest(int_col):
-	return stats.interest_weight(int_col)
+def run_stats(name):
+	name.run_prop()
+	name.run_income()
+	name.run_date()
+	name.run_rank()
+	name.run_interest()
