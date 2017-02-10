331293f7f5145d51627b3b9dfbb7c20652ed148f
bcgilmour <b.c.gilmour@outlook.com>
2017-02-10 14:53:56 -0700

    Update run.py

--- run.py
+++ run.py
@@ -1,10 +1,14 @@
 import 'initialisation.py' as i
 
+d = {}
+xlsx = 0
+options = 0
+
 # importing data
 
 def define(file, name):
 	i.xlsx_import(file)
-	d = i.pop_dict(xlsx, options)
+	d = i.pop_dict(xlsx, options, d)
 	name = i.Selection(d)
 	run_stats(name)
 	print(name.prop)
