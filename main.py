21b21bf65d2fd95a4925d95cea8ef81a24152234
bcgilmour <b.c.gilmour@outlook.com>
2017-02-10 15:42:42 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import "run.py" as r
+
+file = 0
+name = 0
+
+def execute():
+	file = str(input("What file would you like to analyse?: "
+	name = input("What name would you like to give this selection?: "
+	
