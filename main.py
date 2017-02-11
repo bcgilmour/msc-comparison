ab8197a5697e8ed6727954c327095270177ff763
bcgilmour <b.c.gilmour@outlook.com>
2017-02-10 18:57:20 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 import "run.py" as r
 
-file = 0
-name = 0
+file_name = 0
 
 def execute():
-	file = str(input("What file would you like to analyse?: "))
-	name = input("What name would you like to give this selection?: ")
-	r.define(file, name)
+	file_name = str(input("What file would you like to analyse?: "))
+	r.define(file_name)
 	r.reset()
-	file = 0
-	name = 0
+	file_name = 0
